d9abde843b2db03b68f3b273289b2e42490d0b28
mushding_peng <mushding_peng@trendmicro.com>
2023-11-17 14:28:13 +0800

    add sub

--- test.py
+++ test.py
@@ -5,6 +5,9 @@ class CalcClass():
     def add(self, a, b):
         return a + b
     
+    def sub(self, a, b):
+        return a - b
+    
 if __name__ == '__main__':
     calc = CalcClass()
     print(calc.add(1, 2))    
