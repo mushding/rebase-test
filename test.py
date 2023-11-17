2293ce7f5214d6cdc87740a3c2606d2e6a6978fd
mushding_peng <mushding_peng@trendmicro.com>
2023-11-17 14:43:49 +0800

    add div

--- test.py
+++ test.py
@@ -11,6 +11,9 @@ class CalcClass():
     def mul(self, a, b):
         return a * b
     
+    def div(self, a, b):
+        return a / b
+
 if __name__ == '__main__':
     calc = CalcClass()
     print(calc.add(1, 2))    
