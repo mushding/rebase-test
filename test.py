e7f5fd88dd83811307f93d8cb3a758e136ee43ec
mushding_peng <mushding_peng@trendmicro.com>
2023-11-17 14:22:44 +0800

    add add function

---
+++ test.py
@@ -0,0 +1,10 @@
+class CalcClass():
+    def __init__(self) -> None:
+        pass
+
+    def add(self, a, b):
+        return a + b
+    
+if __name__ == '__main__':
+    calc = CalcClass()
+    print(calc.add(1, 2))    
